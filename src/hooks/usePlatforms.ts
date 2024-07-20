9a9ac17bd7f59fec47f65d05376be5c329f7744f
Konstantinos K <raistlindragonl@gmail.com>
2024-07-21 00:37:04 +0300

    Ship genres and platforms as static data with the app

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,4 +1,4 @@
-import useData from "./useData";
+import plarforms from "../data/platforms";
 
 export interface Platform {
   id: number;
@@ -6,6 +6,6 @@ export interface Platform {
   slug: string;
 }
 
-const usePlatforms = () => useData<Platform>("/platforms/lists/parents");
+const usePlatforms = () => ({ data: plarforms, isLoading: false, error: null });
 
 export default usePlatforms;
