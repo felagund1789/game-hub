d3d9a3584f45d9cf92b31eeec318c24811de6fbf
Konstantinos K <raistlindragonl@gmail.com>
2024-08-09 19:46:27 +0300

    Change logo to back icon when in game details page

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,15 +1,22 @@
-import { HStack, Image } from "@chakra-ui/react";
+import { Box, HStack, Icon, Image } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import { BsChevronLeft } from "react-icons/bs";
 
 const NavBar = () => {
+  const { pathname } = useLocation();
+  const img =
+    pathname.indexOf("games/") > -1 ? (
+      <Icon boxSize="60px" paddingTop={4}><BsChevronLeft /></Icon>
+    ) : (
+      <Image src={logo} boxSize="60px" objectFit="cover" />
+    );
+
   return (
     <HStack padding="10px">
-      <Link to="/">
-        <Image src={logo} boxSize="60px" objectFit="cover" />
-      </Link>
+      <Link to="/">{img}</Link>
       <SearchInput />
       <ColorModeSwitch />
     </HStack>
