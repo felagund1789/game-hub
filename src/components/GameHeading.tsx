34d580e9ab5ca45b3fcf1c3d348d0650476c13d5
Konstantinos K <raistlindragonl@gmail.com>
2024-08-09 20:37:38 +0300

    Update GameHeading to include search term

--- src/components/GameHeading.tsx
+++ src/components/GameHeading.tsx
@@ -4,13 +4,16 @@ import usePlatform from "../hooks/usePlatform";
 import useGameQueryStore from "../store";
 
 const GameHeading = () => {
+  const searchText = useGameQueryStore((s) => s.gameQuery.searchText);
   const genreId = useGameQueryStore((s) => s.gameQuery.genreId);
   const genre = useGenre(genreId);
-  
+
   const platformId = useGameQueryStore((s) => s.gameQuery.platformId);
   const platform = usePlatform(platformId);
 
-  const heading = `${platform?.name || ""} ${genre?.name || ""} Games`;
+  const heading = `${platform?.name || ""} ${genre?.name || ""} Games ${
+    searchText ? " for \"" + searchText + "\"" : ""
+  }`;
 
   return (
     <Heading as="h1" marginY={5} fontSize="5xl">
