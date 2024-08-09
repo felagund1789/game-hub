b849b09b61b6cbc68739c7fffabc0447662efb07
Konstantinos K <raistlindragonl@gmail.com>
2024-08-09 17:19:37 +0300

    Build game trailer

---
+++ src/hooks/useTrailers.ts
@@ -0,0 +1,16 @@
+import { useQuery } from "@tanstack/react-query";
+import APIClient from "../services/api-client";
+import { Trailer } from "../entities/Trailer";
+import ms from "ms";
+
+const useTrailers = (gameId: number) => {
+  const apiClient = new APIClient<Trailer>(`games/${gameId}/movies`);
+
+  return useQuery({
+    queryKey: ["trailers", gameId],
+    queryFn: apiClient.getAll,
+    staleTime: ms("24h"),
+  });
+};
+
+export default useTrailers;
