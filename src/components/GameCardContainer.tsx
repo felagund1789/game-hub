02489896107c595e50a91f0c9bedd1cd49b5b50e
Konstantinos K <raistlindragonl@gmail.com>
2024-08-09 14:50:19 +0300

    Fetch game details

--- src/components/GameCardContainer.tsx
+++ src/components/GameCardContainer.tsx
@@ -5,7 +5,18 @@ interface Props {
   children: ReactNode;
 }
 const GameCardContainer = ({ children }: Props) => {
-  return <Box borderRadius={10} overflow="hidden">{ children }</Box>;
+  return (
+    <Box
+      _hover={{
+        transform: "scale(1.03)",
+        transition: "transform .15s ease-in",
+      }}
+      borderRadius={10}
+      overflow="hidden"
+    >
+      {children}
+    </Box>
+  );
 };
 
 export default GameCardContainer;
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -2,7 +2,7 @@ import axios, { AxiosRequestConfig } from "axios";
 
 export interface FetchResponse<T> {
   count: number;
-  next: string | null; 
+  next: string | null;
   results: T[];
 }
 
@@ -23,6 +23,12 @@ class APIClient<T> {
   getAll = (config?: AxiosRequestConfig) => {
     return axiosInstance
       .get<FetchResponse<T>>(this.endpoint, config)
+      .then((res) => res.data);
+  };
+
+  get = (id: number | string) => {
+    return axiosInstance
+      .get<T>(`${this.endpoint}/${id}`)
       .then((res) => res.data);
   };
 }
